d9e4c683ab3d36a2830650010ccbd7192f41258d
synhya <synhya@yeouidopartners.com>
2024-10-03 23:06:35 +0900

    cache clear

--- src/api/stream/streamRouter.ts
+++
@@ -1,7 +0,0 @@
-import { createApiResponse } from "@/api-docs/openAPIResponseBuilders";
-import { OpenAPIRegistry } from "@asteasolutions/zod-to-openapi";
-import express from "express";
-import { z } from "zod";
-
-export const streamRegistry = new OpenAPIRegistry();
-export const streamRouter = express.Router();
